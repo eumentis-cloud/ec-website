ebae4a1c553369d65c19261bce9688df7d660194
eumentis-aditya <aditya@eumentis.com>
2020-10-26 16:04:58 +0530

    updated invalid route

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -25,7 +25,7 @@ const Header: React.FC<LocationProps> = ({ location }) => {
       <div className={`col-auto primary-nav-item ${location.pathname === '/Services/DigitalTransformation/' ? 'primary-nav-active' : ''}`}>
         <Link to={"/Services/DigitalTransformation/"} className="nav-item" activeClassName="nav-item active">Our Services</Link>
         {location.pathname === '/Services/DigitalTransformation/' || location.pathname === '/Services/AppDev/' || location.pathname === '/Services/Startups/' || location.pathname === '/Services/DevOps/' ?
-            <div style={{top: '1.65rem', paddingLeft: 14, position: 'absolute', fontSize: '1.2rem', color: location.pathname === '/Services/Startups/' ? '#FFF' : '#FFFF66' }}>
+            <div style={{top: '1.65rem', paddingLeft: 28, position: 'absolute', fontSize: '1.2rem', color: location.pathname === '/Services/Startups/' ? '#FFF' : '#FFFF66' }}>
               <CaretIcon />
             </div> : null
         }
@@ -36,7 +36,7 @@ const Header: React.FC<LocationProps> = ({ location }) => {
           location.pathname === '/AboutUs/' || location.pathname === '/OurStoryOurMission/' || location.pathname === '/Team/' ? 'primary-nav-active' : ''
         }`}
       >
-          <Link className="nav-item" activeClassName="nav-item active" to={'/AboutUs/aaa/'}>About Us</Link>
+          <Link className="nav-item" activeClassName="nav-item active" to={'/AboutUs/'}>About Us</Link>
         {location.pathname === '/AboutUs/' ?
             <div style={{top: '1.65rem', paddingLeft: 14, position: 'absolute', fontSize: '1.2rem', color: '#FFFF66'}}>
               <CaretIcon />
---
+++ src/utils/globals.ts
@@ -0,0 +1,4 @@
+// global data declaration
+
+// message for per-project apps that are internally distributed
+export const unlistedAppsMessage: string = "The mobile apps are distributed internally.";
