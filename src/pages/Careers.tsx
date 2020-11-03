65e319ad6dc87ede902f410c81366b2fed1ed0f6
eumentis-aditya <aditya@eumentis.com>
2020-11-03 15:45:06 +0530

    organised file structure for careers page

---
+++ src/pages/Careers.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {LocationProps} from "../../utils/types";
+import Layout from "../../layouts/Layout";
+import '../../scss/careers.scss';
+import CareersLayout from "../../layouts/CareersLayout";
+import {Link} from 'gatsby';
+
+// FC
+const Careers: React.FC<LocationProps> = ({location}) => {
+
+    return (
+        <Layout location={location} className="px-0 mx-0 overflow-hidden">
+            <CareersLayout sectionTitle={'Apply for software development positions with Eumentis Informatics Pvt. Ltd.'} children={
+                <section>
+                    <div className="heading">Available positions</div>
+                    <div className="contents">
+                        <ul>
+                            <li>
+                                <Link className="link-for-position" to={'/Careers/SeniorDeveloper/'}>Senior Software Developer</Link>
+                            </li>
+                            <li>
+                                <Link className="link-for-position" to={'/Careers/JuniorDeveloper/'}>Junior Software Developer</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+            }  />
+        </Layout>
+    )
+}
+
+export default Careers;
---
+++ src/pages/Careers/JuniorDeveloper.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {LocationProps} from "../utils/types";
+import Layout from "../layouts/Layout";
+import '../scss/careers.scss';
+import CareersLayout from "../layouts/CareersLayout";
+
+// FC
+const JuniorDeveloper: React.FC<LocationProps> = ({location}) => {
+
+    return (
+        <Layout location={location} className="px-0 mx-0 overflow-hidden">
+            <CareersLayout sectionTitle={'Apply for software development positions with Eumentis Informatics Pvt. Ltd.'} children={
+                <section>
+                    <div className="heading">Available positions</div>
+                    <div className="contents">
+                        <ul>
+                            <li>
+                                <a className="link-for-position" href="seniorDeveloper.html"
+                                >Senior Software Developer</a
+                                >
+                            </li>
+                            <li>
+                                <a className="link-for-position" href="juniorDeveloper.html"
+                                >Junior Software Developer</a
+                                >
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+            }  />
+        </Layout>
+    )
+}
+
+export default JuniorDeveloper;
