8b5774146c07fa76f5879277164adb14120f9d0a
eumentis-aditya <aditya@eumentis.com>
2020-12-08 20:37:16 +0530

    fixed  issue #22 - Main Menu items' hyperlink

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,73 +11,75 @@ const Header: React.FC<LocationProps> = ({ location }) => {
       id="primary-nav"
       className="row justify-content-center yellow-triangle"
     >
-      <div
-        className={`col-auto primary-nav-item ${
-          location.pathname === '/' ? 'primary-nav-active' : ''
-        }`}
-      >
-          <Link className="nav-item" activeClassName="nav-item active" to={'/'}>Home</Link>
+        <Link to={"/"} className={`col-auto primary-nav-item ${
+            location.pathname === '/' ? 'primary-nav-active' : ''
+        }`}>
+          <span className="nav-item">Home</span>
         {location.pathname === '/' ?
             <div style={{top: '1.65rem', paddingLeft: 8, position: 'absolute', fontSize: '1.2rem', color: '#FFF'}}>
               <CaretIcon />
             </div> : null}
-      </div>
-      <div className={`col-auto primary-nav-item ${location.pathname === '/Services/DigitalTransformation/' || location.pathname === '/Services/AppDev/' || location.pathname === '/Services/DevOps/' || location.pathname === '/Services/Startups/' ? 'primary-nav-active' : ''}`}>
-        <Link to={"/Services/DigitalTransformation/"} className="nav-item" activeClassName="nav-item active">Our Services</Link>
+        </Link>
+      <Link to={"/Services/DigitalTransformation/"} className={`col-auto primary-nav-item ${location.pathname === '/Services/DigitalTransformation/' || location.pathname === '/Services/AppDev/' || location.pathname === '/Services/DevOps/' || location.pathname === '/Services/Startups/' ? 'primary-nav-active' : ''}`}>
+        <span className="nav-item">Our Services</span>
         {location.pathname === '/Services/DigitalTransformation/' || location.pathname === '/Services/AppDev/' || location.pathname === '/Services/Startups/' || location.pathname === '/Services/DevOps/' ?
             <div style={{top: '1.65rem', paddingLeft: 28, position: 'absolute', fontSize: '1.2rem', color: location.pathname === '/Services/Startups/' ? '#FFF' : '#FFFF66' }}>
               <CaretIcon />
             </div> : null
         }
-      </div>
+      </Link>
 
-      <div
+      <Link
+          to={"/OurWork/"}
           className={`col-auto primary-nav-item ${
               location.pathname === '/OurWork/' ? 'primary-nav-active' : ''
           }`}
       >
-        <Link className="nav-item" activeClassName="nav-item active" to={'/OurWork/'}>Our Work</Link>
+        <span className="nav-item">Our Work</span>
         {location.pathname === '/OurWork/' ?
             <div style={{top: '1.65rem', paddingLeft: 16, position: 'absolute', fontSize: '1.2rem', color: '#FFF'}}>
               <CaretIcon />
             </div> : null}
-      </div>
+      </Link>
 
-      <div
+      <Link
+        to={'/AboutUs/'}
         className={`col-auto primary-nav-item ${
           location.pathname === '/AboutUs/' || location.pathname === '/OurStoryOurMission/' || location.pathname === '/Team/' ? 'primary-nav-active' : ''
         }`}
       >
-          <Link className="nav-item" activeClassName="nav-item active" to={'/AboutUs/'}>About Us</Link>
+          <span className="nav-item">About Us</span>
         {location.pathname === '/AboutUs/' ?
             <div style={{top: '1.65rem', paddingLeft: 14, position: 'absolute', fontSize: '1.2rem', color: '#FFFF66'}}>
               <CaretIcon />
             </div> : null}
-      </div>
+      </Link>
 
-      <div
+      <Link
+          to={'/Careers/'}
           className={`col-auto primary-nav-item ${
               location.pathname === '/Careers/' ? 'primary-nav-active' : ''
           }`}
       >
-        <Link className="nav-item" activeClassName="nav-item active" to={'/Careers/'}>Careers</Link>
+        <span className="nav-item">Careers</span>
         {location.pathname === '/Careers/' ?
             <div style={{top: '1.65rem', paddingLeft: 12, position: 'absolute', fontSize: '1.2rem', color: '#FFFF66'}}>
               <CaretIcon />
             </div> : null}
-      </div>
+      </Link>
 
-      <div
+      <Link
+        to={'/ContactUs/'}
         className={`col-auto primary-nav-item ${
           location.pathname === '/ContactUs/' ? 'primary-nav-active' : ''
         }`}
       >
-        <Link className="nav-item" activeClassName="nav-item active" to={'/ContactUs/'}>Contact Us</Link>
+        <span className="nav-item">Contact Us</span>
         {location.pathname === '/ContactUs/' ?
             <div style={{top: '1.65rem', paddingLeft: 18, position: 'absolute', fontSize: '1.2rem', color: '#FFF'}}>
               <CaretIcon />
             </div> : null}
-      </div>
+      </Link>
     </nav>
   )
 }
