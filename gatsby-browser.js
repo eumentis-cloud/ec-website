fa5abfafcd053dc1a77bff1e54e7b119f9275dbf
eumentis-aditya <aditya@eumentis.com>
2021-05-06 21:35:18 +0530

    fixed layout, spacing issues as per bootstrap major version update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,10 @@
 import 'animate.css/animate.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import '@popperjs/core/dist/umd/popper.min.js';
+import 'bootstrap/js/src/util';
+import 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/scss/bootstrap.scss';
+import 'bootstrap/scss/bootstrap-reboot.scss';
+import 'bootstrap/scss/bootstrap-grid.scss';
+import 'bootstrap/scss/bootstrap-utilities.scss';
